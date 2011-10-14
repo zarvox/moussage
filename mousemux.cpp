f97b0d56af83ff159108242d855683dc6954f412
Drew Fisher <drew.m.fisher@gmail.com>
2011-10-14 13:02:47 -0700

    Make output based upon mouse input, not repeated "Test\n"
    Signed-off-by: Drew Fisher <drew.m.fisher@gmail.com>

--- mousemux.cpp
+++ mousemux.cpp
@@ -10,13 +10,26 @@ MouseMux::MouseMux(QObject* parent) : QObject(parent) {
 	pb->show();
 	QObject::connect(pb, SIGNAL(clicked()),
 				qApp, SLOT(quit()));
-	
+
 	// Init the serial port we'll talk to the Arduino over.
 	sp = new SerialPort();
 	serial_enabled = sp->open();
 }
 
 MouseMux::~MouseMux() {
+}
+
+static int region(QByteArray data, int rowbegin, int colbegin, int rowend, int colend) {
+	// Note: end values are INCLUSIVE unlike most Qt classes
+	int tally = 0;
+	int pixels_used = 0;
+	for(int row = rowbegin; row <= rowend; row++) {
+		for(int col = colbegin ; col <= colend; col++) {
+			tally += data[15 * row + col];
+			pixels_used++;
+		}
+	}
+	return (tally / pixels_used);
 }
 
 void MouseMux::imageUpdate(qint64 id, QByteArray data) {
@@ -34,6 +47,30 @@ void MouseMux::imageUpdate(qint64 id, QByteArray data) {
 	widgetMap[id]->updateImage(data);
 	if (serial_enabled) {
 		qDebug() << "Writing to serial";
-		qDebug() << sp->write("Test\n");
+		QByteArray serialdata;
+		serialdata.resize(12);
+		// do something to map from image to LED brightness
+		// Zones are numbered: 
+		//       0 6
+		//       1 7
+		//       2 8
+		//       3 9
+		//   5   4 10  11		
+		serialdata[0] = (char)region(data, 0, 3, 2, 7);
+		serialdata[1] = (char)region(data, 3, 3, 5, 7);
+		serialdata[2] = (char)region(data, 5, 3, 7, 7);
+		serialdata[3] = (char)region(data, 7, 3, 9, 7);
+		serialdata[4] = (char)region(data, 10, 3, 12, 7);
+		serialdata[5] = (char)region(data, 10, 0, 12, 3);
+		serialdata[6] = (char)region(data, 0, 7, 2, 11);
+		serialdata[7] = (char)region(data, 3, 7, 5, 11);
+		serialdata[8] = (char)region(data, 5, 7, 7, 11);
+		serialdata[9] = (char)region(data, 7, 7, 9, 11);
+		serialdata[10] = (char)region(data, 10, 7, 12, 11);
+		serialdata[11] = (char)region(data, 10, 11, 12, 14);
+		
+		int written = sp->write(serialdata);
+		qDebug() << written << "byte written";
+		//qDebug() << sp->write("Test\n");
 	}
 }
